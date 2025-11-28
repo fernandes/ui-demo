fda406b93409f744c04c486b15aca355629426d0
Celso Fernandes <celso.fernandes@gmail.com>
2025-11-28 14:57:10 -0300

    register stimulus controllers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,7 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+// Register UI Engine controllers
+import { registerControllers } from "ui"
+registerControllers(application)
