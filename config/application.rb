dbe39152be107936d17956e2503b5142801f8468
Celso Fernandes <celso.fernandes@gmail.com>
2025-12-03 22:39:19 -0300

    remove config hosts

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module UiDemo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << "carbon:4001"
   end
 end
